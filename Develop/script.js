efd3728047fd7864d509011c8f56a503f2c138ef
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-02-21 19:59:26 -0600

    created prompts and variables for user answers

---
+++ Develop/script.js
@@ -0,0 +1,63 @@
+
+var userLength = prompt("How many characters do you want in your password? (8-128)");
+var userUpperCase = confirm("Would you like your password to have uppercase letters?");
+var userLowerCase = confirm("Would you like your password to have lowercase letters?");
+var userNumbers = confirm("Would you like your password to have numbers?");
+var userCharacters = confirm("Would you like your password to have any special characters?");
+
+
+if (userUpperCase) {
+  upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+} else {
+  upCase = "";
+};
+
+if (userLowerCase) {
+  lowCase = "abcdefghijklmnopqrstuvwxyz";
+} else {
+  lowCase = "";
+};
+
+if (userNumbers) {
+  num = "1234567890";
+} else {
+  num = "";
+};
+
+if (userCharacters) {
+  char = "!@#$%&?/^*";
+} else {
+  char = "";
+};
+
+
+var choices = {
+  upperCase: upCase,
+  lowerCase: lowCase,
+  numbers: num,
+  characters: char
+}
+
+console.log(choices);
+
+
+
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
